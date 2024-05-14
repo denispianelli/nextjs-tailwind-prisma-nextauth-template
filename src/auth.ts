fb2fef600adabec91c5994f838881f3a7171f368
Denis Pianelli <denis.pianelli@gmail.com>
2024-05-14 10:20:54 +0200

    chore: Update auth.ts with user id token handling

--- src/auth.ts
+++ src/auth.ts
@@ -53,4 +53,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       allowDangerousEmailAccountLinking: true,
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        // User is available during sign-in
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
 });
