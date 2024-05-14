2c878d2f3453d27b242d765f26ed9acb940f8933
Denis Pianelli <denis.pianelli@gmail.com>
2024-05-14 10:21:29 +0200

    chore: Add UploadButton and UploadDropzone components for Uploadthing integration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from '@uploadthing/react';
+
+import type { OurFileRouter } from '@/app/api/uploadthing/core';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
