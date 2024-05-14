eb3a7b3a07c554dde4c4310ca46f0101cdb0cec9
Denis Pianelli <denis.pianelli@gmail.com>
2024-05-14 19:53:01 +0200

    chore: Update page title alignment in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-[calc(100vh-64px)] items-center justify-center bg-muted/40">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
         Welcome to your new app!
       </h1>
     </main>
