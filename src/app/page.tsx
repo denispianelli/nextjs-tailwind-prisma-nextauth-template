94ca0b1edb40b489d67cc727637d90912be6349b
Denis Pianelli <denis.pianelli@gmail.com>
2024-05-15 13:59:02 +0200

    feat: Add copy-to-clipboard functionality for create-next-app command

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
+'use client';
+
+import { Alert, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import clsx from 'clsx';
+import { Check, Copy, Terminal } from 'lucide-react';
+import { useState } from 'react';
+
 export default function Home() {
+  const [copied, setCopied] = useState(false);
+
+  const text = `npx create-next-app -e
+	https://github.com/denispianelli/nextjs-tailwind-prisma-nextauth-template`;
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
   return (
-    <main className="flex h-[calc(100vh-64px)] items-center justify-center bg-muted/40">
+    <main className="flex h-[calc(100vh-64px)] flex-col items-center justify-center bg-muted/40">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
         Welcome to your new app!
       </h1>
+      <Alert className="mt-6  w-auto">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle className="text-sm">
+          {' '}
+          ~ npx create-next-app -e
+          https://github.com/denispianelli/nextjs-tailwind-prisma-nextauth-template
+          <Button variant="default" onClick={copyToClipboard} className="ml-4">
+            <Copy
+              className={clsx('h-4 w-4 ease-in-out', {
+                hidden: copied,
+              })}
+            />
+            <Check
+              className={clsx('h-4 w-4 ease-in-out', {
+                hidden: !copied,
+              })}
+            />
+          </Button>
+        </AlertTitle>
+      </Alert>
     </main>
   );
 }
