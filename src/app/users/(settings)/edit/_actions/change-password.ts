7352b5144a1ace7c54d02ff1c2d1884a82973ded
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-05 11:21:26 +0200

    refactor: Update change password functionality in user settings edit page

--- src/app/users/(settings)/edit/_actions/change-password.ts
+++ src/app/users/(settings)/edit/_actions/change-password.ts
@@ -2,77 +2,34 @@
 
 import db from '@/db/prisma';
 import { notFound, redirect } from 'next/navigation';
-import { z } from 'zod';
 import bcrypt from 'bcryptjs';
-import { revalidatePath } from 'next/cache';
 
-const ChangePasswordSchema = z.object({
-  currentPassword: z
-    .string()
-    .min(1, { message: 'Password is required.' })
-    .trim(),
-  newPassword: z
-    .string()
-    .min(1, { message: 'Password is required.' })
-    .min(8, { message: 'Be at least 8 characters long.' })
-    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-    .regex(/[0-9]/, { message: 'Contain at least one number.' })
-    .regex(/[^\w]/, { message: 'Contain at least one special character.' })
-    .trim(),
-  confirmPassword: z
-    .string()
-    .min(1, { message: 'You must confirm your password.' })
-    .trim(),
-});
-
-export type ChangePasswordFormState =
-  | {
-      errors?: {
-        currentPassword?: string[];
-        newPassword?: string[];
-        confirmPassword?: string[];
-      };
-    }
-  | undefined;
+type ChangePasswordForm = {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+};
 
 export async function changePassword(
+  changePasswordForm: ChangePasswordForm,
   id: string,
-  prevState: ChangePasswordFormState,
-  formData: FormData,
 ) {
-  const validatedFields = ChangePasswordSchema.safeParse({
-    currentPassword: formData.get('currentPassword'),
-    newPassword: formData.get('newPassword'),
-    confirmPassword: formData.get('confirmPassword'),
-  });
-
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-    };
-  }
-
-  const { currentPassword, newPassword, confirmPassword } =
-    validatedFields.data;
+  const { currentPassword, newPassword, confirmPassword } = changePasswordForm;
 
   try {
     const user = await db.user.findUnique({
       where: { id },
     });
-
     if (!user) return notFound();
-
     const isValidPassword =
       user.password && (await bcrypt.compare(currentPassword, user.password));
-
     if (!isValidPassword) {
       return {
         errors: {
-          currentPassword: ['Current password is incorrect.'],
+          currentPassword: 'Your current password is incorrect.',
         },
       };
     }
-
     if (newPassword !== confirmPassword) {
       return {
         errors: {
@@ -80,9 +37,7 @@ export async function changePassword(
         },
       };
     }
-
     const hashedPassword = await bcrypt.hash(newPassword, 10);
-
     await db.user.update({
       where: {
         id,
@@ -96,7 +51,6 @@ export async function changePassword(
       message: 'Database Error: Failed to update password.',
     };
   }
-
   return {
     message: 'Password updated successfully.',
   };
