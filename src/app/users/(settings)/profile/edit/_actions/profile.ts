e1f25d412051055ab41eea8dc148cc0c60fdd8d2
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-05 11:22:22 +0200

    refactor: Update user settings edit page to use profileForm in updateProfile function

--- src/app/users/(settings)/profile/edit/_actions/profile.ts
+++ src/app/users/(settings)/profile/edit/_actions/profile.ts
@@ -1,53 +1,17 @@
 'use server';
 
 import db from '@/db/prisma';
-import { z } from 'zod';
 import { notFound } from 'next/navigation';
 import { isEmailAvailable } from '@/app/sign_up/_actions/sign-up';
 
-const updateProfileSchema = z.object({
-  firstname: z
-    .string()
-    .min(2, { message: 'At least 2 characters.' })
-    .max(255)
-    .optional()
-    .or(z.literal('')),
-  lastname: z
-    .string()
-    .min(2, { message: 'At least 2 characters.' })
-    .max(255)
-    .optional()
-    .or(z.literal('')),
-  email: z.string().email(),
-});
+type ProfileForm = {
+  firstname?: string;
+  lastname?: string;
+  email: string;
+};
 
-export type UpdateProfileFormState =
-  | {
-      errors?: {
-        firstname?: string[];
-        lastname?: string[];
-        email?: string[];
-      };
-      message?: string;
-    }
-  | undefined;
-
-export async function updateProfile(
-  id: string,
-  prevState: UpdateProfileFormState,
-  formData: FormData,
-) {
-  const validateFields = updateProfileSchema.safeParse(
-    Object.fromEntries(formData.entries()),
-  );
-
-  if (!validateFields.success) {
-    return {
-      errors: validateFields.error.flatten().fieldErrors,
-    };
-  }
-
-  const { firstname, lastname, email } = validateFields.data;
+export async function updateProfile(profileForm: ProfileForm, id: string) {
+  const { firstname, lastname, email } = profileForm;
 
   const isEmailFree = await isEmailAvailable(email);
 
